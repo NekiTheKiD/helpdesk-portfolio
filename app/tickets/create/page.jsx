996e5545a34bb516d88d2b882b7095390821f2c1
nekithekid <Franek@MacBook-Pro-de-Franek.local>
2023-11-17 14:10:38 +0100

    finishing project

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from './CreateForm';
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className='text-primary text-center'>Ajouter un nouveau ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
